f2181da80e66395b2cd55cde5c4de2f59b2ae88e
BALDE <balde@localhost.localdomain>
2019-02-17 18:33:18 +0100

    interface dao

---
+++ source/src/main/java/com/fligth_sharing/dao/Dao.java
@@ -0,0 +1,51 @@
+package com.fligth_sharing.dao;
+
+import java.util.List;
+
+public interface Dao {
+
+	/**
+	 * 
+	 * @return return all the contents of the table
+	 * @throws Exception
+	 */
+	public List<String> getAll() throws Exception;
+
+	/**
+	 * select * from ? where id=id
+	 * 
+	 * @param id the Id of the object we're looking for
+	 * @return return the object corresponding to the id
+	 * @throws Exception
+	 */
+	public String getByID(String id) throws Exception;
+
+	/**
+	 * delete from ? where id=id
+	 * 
+	 * @param id object's id
+	 * @return success or error message
+	 * @throws Exception
+	 */
+	public String delete(String id) throws Exception;
+
+	/**
+	 * Add an object in the database
+	 * 
+	 * @param json
+	 * @param id   object's id
+	 * @return success or error message
+	 * @throws Exception
+	 */
+	public String add(byte[] json, String id) throws Exception;
+
+	/**
+	 * update set ?=? where id=id
+	 * 
+	 * @param id        object's id
+	 * @param attribute
+	 * @param valeur
+	 * @throws Exception
+	 */
+	public void update(String id, String attribute, String value) throws Exception;
+}
