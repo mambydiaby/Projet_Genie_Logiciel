308cfba58f3197a79867888bb54a35fb204c9d55
mybalde <mamadouyero95@gmail.com>
2019-03-07 12:35:32 +0100

    flight websersice & html,js test

---
+++ source/src/main/webapp/js/booking.js
@@ -0,0 +1,16 @@
+function mysubmit() {
+	var departure = $('#departure').val();
+	var date = $('#date').val();
+	$.ajax({
+		type: "post",
+		url: '/ws/flight/search',
+		data: {
+			'departure': departure,
+			'date': date
+		},
+		dataType: "json",
+		success: function(data) {
+			alert(data);
+		}
+	})
+}
