cc9d60d21b9810ec2c5c51772993ec758dd02f22
mybalde <mamadouyero95@gmail.com>
2019-03-15 07:46:47 +0100

    new dao craetion

--- source/src/main/java/com/flight_sharing/dao/FactoryDao.java
+++ source/src/main/java/com/flight_sharing/dao/FactoryDao.java
@@ -1,11 +1,14 @@
 package com.flight_sharing.dao;
 
 public class FactoryDao {
-	public static final int FLIGHT=1;
-	public static final int PILOT=2;
-	public static final int PASSENGER=3;
+	public static final int FLIGHT = 1;
+	public static final int PILOT = 2;
+	public static final int PASSENGER = 3;
+	public static final int AIRPORT = 4;
+	public static final int RESERVATION = 5;
+	public static final int AIRPLANE = 6;
 
-	public static ActionDao createDAO(int id){
+	public static ActionDao createDAO(int id) {
 		switch (id) {
 		case FLIGHT:
 			return new ActionDao("flight");
@@ -13,6 +16,12 @@ public class FactoryDao {
 			return new ActionDao("pilote");
 		case PASSENGER:
 			return new ActionDao("passenger");
+		case AIRPORT:
+			return new ActionDao("airport");
+		case RESERVATION:
+			return new ActionDao("reservation");
+		case AIRPLANE:
+			return new ActionDao("airplane");
 		default:
 			return null;
 		}
