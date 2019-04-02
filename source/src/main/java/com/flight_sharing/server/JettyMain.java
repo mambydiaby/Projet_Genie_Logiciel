317b879f74ec14d27a7be99bad062136a7f02c9d
LINGJIARONG <415447764@qq.com>
2019-04-02 17:19:01 +0200

    new fonctionalities

--- source/src/main/java/com/flight_sharing/server/JettyMain.java
+++ source/src/main/java/com/flight_sharing/server/JettyMain.java
@@ -24,6 +24,7 @@ import com.flight_sharing.reminder.Reminder;
 public class JettyMain {
 
 	public static void main(String[] args) throws Exception {
+		
 		//prefill data
 		FillData.fill();
 		// Initialize the server
