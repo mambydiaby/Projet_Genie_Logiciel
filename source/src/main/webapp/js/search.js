05035c0ee5821ee49220863ad5dbd75bb93f48f0
LINGJIARONG <415447764@qq.com>
2019-04-15 11:15:31 +0200

    corection bug

--- source/src/main/webapp/js/search.js
+++ source/src/main/webapp/js/search.js
@@ -117,7 +117,7 @@ function filter() {
 				var td2 = "<td>" + moment(obj.date).format("MMMM Do YYYY") + "</td>";
 				var td3 = "<td>" + obj.seat + "</td>";
 				var td4 = "<td>" + obj.price + "</td>";
-				var td5 = "<td class=\"text-center\"><a class='btn btn-info btn-xs' href=\"#\"><span class=\"glyphicon glyphicon-ok\"></span> See</a> </td></tr>";
+				var td5 = "<td class=\"text-center\"><a onclick=\"storing('"+obj.id+"');\" target=\"_blank\" class='btn btn-info btn-xs' href=\"../flight_detail.html\" ><span class=\"glyphicon glyphicon-ok\"></span> See</a> </td></tr>"
 				tbl += td0 + td1 +td1_5+ td2 + td3 + td4 + td5;
 
 				
