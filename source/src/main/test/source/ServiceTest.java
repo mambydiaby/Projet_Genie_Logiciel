63dee26380e4784fbdb75532a8a5bedf26331178
mybalde <mamadouyero95@gmail.com>
2019-04-29 09:25:40 +0200

    test

--- source/src/main/test/source/ServiceTest.java
+++ source/src/main/test/source/ServiceTest.java
@@ -1,39 +1,25 @@
 package source;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import org.json.JSONObject;
-import org.junit.Before;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.junit.Test;
 
-import com.flight_sharing.entities.Passenger;
-import com.flight_sharing.entities.Pilot;
-import com.flight_sharing.ws.UserService;
-
 public class ServiceTest {
-	private UserService userService;
-	private Passenger p;
-	private Pilot pl;
-
-	@Before
-	public void initialization() {
-		p = new Passenger("azerty0", "544", "Balde", "mamadou", "mybalde@test.com");
-		pl = new Pilot("007", "fr021", "james", "bond", "test@junit.com", "test", "teesst");
-	}
-
-	@Test
-	public void addUserTest() {
-		System.out.println(userService.addPassenger(p));
-		JSONObject j1 = new JSONObject(userService.addPassenger(p)), j2 = new JSONObject(userService.addPilote(pl));
-		System.out.println("********************************"+j1.get("result")+"**********************************");
-		assertTrue(j1.get("result").equals("ok"));
-		assertTrue(j2.get("result").equals("ok"));
-	}
 	
 	@Test
-	public void deleteTest() {
-		assertEquals("OK", userService.delete(p.getId()));
-		assertEquals("OK", userService.delete(pl.getId()));
-	}
+	public void givenCreateFruit_whenFormContainsNullParam_thenResponseCodeIsBadRequest() {
+	    Form form = new Form();
+	    form.param("id", "gtx15z");
+	    form.param("pwd", null);
+	    Response response = target("user/login").request(MediaType.APPLICATION_FORM_URLENCODED)
+	        .post(Entity.form(form));
+	 
+	    assertEquals("Http Response should be 400 ", 400, response.getStatus());
+	    assertThat(response.readEntity(String.class), containsString("Fruit colour must not be null"));
+	 }
 }
